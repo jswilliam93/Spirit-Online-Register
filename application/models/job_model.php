eaaa0ba9eb3c541c4014dd118c2bd60ebdcd866c
Joseph Saied William <jswilliam93@gmail.com>
2015-09-16 23:52:38 +0200

    Update and rename Job_model.php to job_model.php

--- application/models/job_model.php
+++ application/models/job_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Job_model extends CI_Model {
+class job_model extends CI_Model {
 
 	public function __construct()
 	{
